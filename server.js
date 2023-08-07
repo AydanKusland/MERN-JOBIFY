70b377e3b450a5f39afd72e797ebeaa3a373920c
AydanKusland <chinatugov@mail.ru>
2023-08-07 19:28:19 +0800

    after 79

--- server.js
+++ server.js
@@ -1,58 +1,39 @@
+import 'express-async-errors'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 const app = express()
-
 import morgan from 'morgan'
+import mongoose from 'mongoose'
+
+// routers
+import jobRouter from './routs/jobRouter.js'
+
+//middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
+
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
-import { nanoid } from 'nanoid'
-
-let jobs = [
-	{ id: nanoid(), company: 'apple', position: 'front-end' },
-	{ id: nanoid(), company: 'google', position: 'back-end' }
-]
-
 app.use(express.json())
 
-// GET ALL JOBS
-app.get('/api/v1/jobs', (req, res) => {
-	res.status(200).json({ jobs })
-})
-app.get('/api/v1/jobs/:id', (req, res) => {
-	const { id } = req.params
-	const job = jobs.find(job => job.id === id)
-	if (!job) return res.status(404).json({ msg: `no job with id ${id}` })
-	res.status(200).json({ job })
-})
-// CREATE A JOB
-app.post('/api/v1/jobs', (req, res) => {
-	const { company, position } = req.body
-	if (!company || !position)
-		return res.status(400).json({ msg: 'please provide company and position' })
-	const id = nanoid(10)
-	const job = { id, company, position }
-	jobs.push(job)
-	res.status(201).json({ job })
-})
-// EDIT JOB
-app.patch('/api/v1/jobs/:id', (req, res) => {
-	const { id } = req.params
-	const job = jobs.find(job => job.id === id)
-	if (!job) return res.status(404).json({ msg: `no job with id ${id}` })
-	const { company, position } = req.body
-	if (!company || !position)
-		return res.status(400).json({ msg: 'please provide company and position' })
+app.use('/api/v1/jobs', jobRouter)
 
-	job.company = company
-	job.position = position
-	res.status(200).json({ msg: 'job modified', job })
+app.use('*', (req, res) => {
+	res.status(404).json({ msg: 'page not found' })
 })
+
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5100
 
-app.listen(port, () => {
-	console.log(`server is listening on port ${port}`)
-})
+try {
+	await mongoose.connect(process.env.MONGO_URL)
+	app.listen(port, () => {
+		console.log(`server is listening on port ${port}`)
+	})
+} catch (error) {
+	console.log(error)
+	process.exit(1)
+}
